d92a33d33be4c81976f8457f7e10a1320c9b23c1
Guillermo Fernandez Gallardo <guille90@users.noreply.github.com>
2018-08-03 02:28:01 -0500

    didnt work

--- balanced numbers/balanced-numbers.go
+++ balanced numbers/balanced-numbers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,11 +15,8 @@ func balancedNumbers(B, L int64, input []int64) []int64 {
 	for i := int64(L - 1); i >= 0; i-- {
 		N10 += input[i] * int64(math.Pow(float64(B), float64(L-1-i)))
 	}
-	fmt.Print(N10)
-
 	var suma int64
 	var count int64
-	fmt.Print("\n")
 	for i := int64(1); i <= N10; i++ {
 		if isBalanced(i, B) {
 			count++
@@ -25,7 +24,8 @@ func balancedNumbers(B, L int64, input []int64) []int64 {
 		}
 
 	}
-	fmt.Print("\n")
+	modulo := 1004535809
+	suma = int64(math.Mod(float64(suma), float64(modulo)))
 	return []int64{count, suma}
 }
 
@@ -38,7 +38,6 @@ func isBalanced(n int64, B int64) bool {
 		signTotal = i + 1
 	}
 	mid := math.Ceil(signTotal / 2)
-	//fmt.Print(signTotal)
 
 	c := n
 	var sign float64
@@ -68,61 +67,61 @@ func isBalanced(n int64, B int64) bool {
 }
 
 func main() {
-	// reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+	reader := bufio.NewReaderSize(os.Stdin, 4048*4048)
 
-	// stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	// checkError(err)
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
 
-	// defer stdout.Close()
+	defer stdout.Close()
 
-	// writer := bufio.NewWriterSize(stdout, 1024*1024)
+	writer := bufio.NewWriterSize(stdout, 4048*4048)
 
-	// BL := strings.Split(readLine(reader), " ")
+	BL := strings.Split(readLine(reader), " ")
 
-	// Btemp, err := strconv.ParseInt(BL[0], 10, 64)
-	// checkError(err)
-	// B := int32(Btemp)
-	// fmt.Print("B: ", B, " ")
+	Btemp, err := strconv.ParseInt(BL[0], 10, 64)
+	checkError(err)
+	B := int64(Btemp)
+	fmt.Print("B: ", B, " ")
 
-	// Ltemp, err := strconv.ParseInt(BL[1], 10, 64)
-	// checkError(err)
-	// L := int32(Ltemp)
-	// fmt.Print("L: ", L, "\n")
+	Ltemp, err := strconv.ParseInt(BL[1], 10, 64)
+	checkError(err)
+	L := int64(Ltemp)
+	fmt.Print("L: ", L, "\n")
 
-	// inputTemp := strings.Split(readLine(reader), " ")
+	inputTemp := strings.Split(readLine(reader), " ")
 
-	// var input []int64
+	var input []int64
 
-	// for i := 0; i < int(L); i++ {
-	// 	inputItem, err := strconv.ParseInt(inputTemp[i], 10, 64)
-	// 	checkError(err)
-	// 	input = append(input, inputItem)
-	// }
-	// fmt.Print(input, "\n")
+	for i := 0; i < int(L); i++ {
+		inputItem, err := strconv.ParseInt(inputTemp[i], 10, 64)
+		checkError(err)
+		input = append(input, inputItem)
+	}
+	fmt.Print(input, "\n")
 
 	// B := int64(10)
 	// input := []int64{7}
 
-	B := int64(10)
-	input := []int64{4, 8, 5, 7}
+	// B := int64(10)
+	// input := []int64{4, 8, 5, 7}
 
 	////-----------------------------
 	// B := int64(11)
 	// input := []int64{1, 10, 9}
 
-	result := balancedNumbers(B, int64(len(input)), input)
+	result := balancedNumbers(B, L, input)
 
-	fmt.Print(result)
+	// fmt.Print(result)
 	////-----------------------------
 
 	//fmt.Print(isBalanced(int64(122), int64(11)))
 	//fmt.Print(isBalanced(int64(11), int64(11)))
 
-	// strResult := strconv.FormatInt(result[0], 10) + " " + strconv.FormatInt(result[1], 10)
+	strResult := strconv.FormatInt(result[0], 10) + " " + strconv.FormatInt(result[1], 10)
 
-	// fmt.Fprintf(writer, "%s\n", strResult)
+	fmt.Fprintf(writer, "%s\n", strResult)
 
-	// writer.Flush()
+	writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
