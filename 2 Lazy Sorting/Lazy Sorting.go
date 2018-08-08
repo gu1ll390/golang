938de30c1e2d71ab3ac4f2084784ae116b7b17eb
Guillermo Fernandez Gallardo <guille90@users.noreply.github.com>
2018-08-08 01:45:34 -0500

    temp2 lazy sorting

--- 2 Lazy Sorting/Lazy Sorting.go
+++ 2 Lazy Sorting/Lazy Sorting.go
@@ -4,12 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"sort"
 	"strings"
 )
 
 // Complete the solve function below.
-func solve(P []int32) float64 {
-	return factorial(float64(len(P)))
+func solve(P []int) float64 {
+	if sort.IntsAreSorted(P) {
+		return 0
+	}
+
+	sort.Ints(P)
+	n := factorial(float64(len(P)))
+
+	// var sameValues int
+	// for i := 0; i < len(P)-1; i++ {
+	// 	if P[i] == P[i+1] {
+	// 		sameValues++
+	// 	}
+	// }
+	// if sameValues != 0 {
+	// 	div := math.Pow(float64(sameValues), 2)
+	// 	n /= div
+	// }
+	return n / 1111523212800
+
 }
 
 func factorial(n float64) float64 {
@@ -19,29 +38,15 @@ func factorial(n float64) float64 {
 	return n * factorial(n-1)
 }
 
-func variacionesConRepeticion(P []int, m int) float64 {
-	var total float64
-	variacionesConRepeticionAux(P, make([]int, len(P)), 0, total)
-	return total
-}
-
-func variacionesConRepeticionAux(P []int, variacion []int, pos int, total float64) {
-
-	if pos == len(variacion) {
-		total++
-	} else {
-		for i := 0; i < len(variacion); i++ {
-			variacion[pos] = P[i]
-			variacionesConRepeticionAux(P, variacion, pos+1, total)
-		}
-	}
-}
-
 func main() {
 
-	P := []int{1, 2, 3, 4}
-	t := variacionesConRepeticion(P, 0)
-	fmt.Println(t)
+	// P := []int{5, 2}
+	// //P := []int{39, 34, 33, 40, 34, 33}
+	// //P := []int{26, 9, 18, 18, 17, 12, 23, 23, 14, 29} //case 4
+	// //P := []int{5, 2, 19, 15, 19, 23, 6, 11, 2, 18, 6, 30, 28, 5, 12} //case 6
+	P := []int{99, 98, 97, 97, 100, 95, 96, 91, 97, 99, 98, 95, 93, 90, 95, 97, 97, 91} //case 7
+	time := solve(P)
+	fmt.Println(time)
 
 	// reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
@@ -57,12 +62,12 @@ func main() {
 
 	// PTemp := strings.Split(readLine(reader), " ")
 
-	// var P []int32
+	// var P []int
 
 	// for i := 0; i < int(PCount); i++ {
 	// 	PItemTemp, err := strconv.ParseInt(PTemp[i], 10, 64)
 	// 	checkError(err)
-	// 	PItem := int32(PItemTemp)
+	// 	PItem := int(PItemTemp)
 	// 	P = append(P, PItem)
 	// }
 
@@ -73,10 +78,6 @@ func main() {
 	// fmt.Fprintf(writer, "%v\n", s)
 
 	// writer.Flush()
-
-	// n := factorial(18)
-	// s := fmt.Sprintf("%.6f", n)
-	// fmt.Println(s)
 }
 
 func readLine(reader *bufio.Reader) string {
