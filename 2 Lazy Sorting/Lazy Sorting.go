a3158641791b2b01d84f4dcda490d8c47ff4f1a6
Guillermo Fernandez Gallardo <guille90@users.noreply.github.com>
2018-08-08 02:14:16 -0500

    temp3 lazy sorting

--- 2 Lazy Sorting/Lazy Sorting.go
+++ 2 Lazy Sorting/Lazy Sorting.go
@@ -16,18 +16,21 @@ func solve(P []int) float64 {
 
 	sort.Ints(P)
 	n := factorial(float64(len(P)))
+	div := float64(1)
+	sameValues := 1
+	for i := 0; i < len(P)-1; i++ {
+		if P[i] == P[i+1] {
+			sameValues++
+		} else {
+			if sameValues != 1 {
+				div = div * factorial(float64(sameValues))
+				sameValues = 1
+			}
+		}
 
-	// var sameValues int
-	// for i := 0; i < len(P)-1; i++ {
-	// 	if P[i] == P[i+1] {
-	// 		sameValues++
-	// 	}
-	// }
-	// if sameValues != 0 {
-	// 	div := math.Pow(float64(sameValues), 2)
-	// 	n /= div
-	// }
-	return n / 1111523212800
+	}
+
+	return n / div
 
 }
 
@@ -44,9 +47,25 @@ func main() {
 	// //P := []int{39, 34, 33, 40, 34, 33}
 	// //P := []int{26, 9, 18, 18, 17, 12, 23, 23, 14, 29} //case 4
 	// //P := []int{5, 2, 19, 15, 19, 23, 6, 11, 2, 18, 6, 30, 28, 5, 12} //case 6
-	P := []int{99, 98, 97, 97, 100, 95, 96, 91, 97, 99, 98, 95, 93, 90, 95, 97, 97, 91} //case 7
-	time := solve(P)
-	fmt.Println(time)
+	// P := []int{99, 98, 97, 97, 100, 95, 96, 91, 97, 99, 98, 95, 93, 90, 95, 97, 97, 91} //case 7
+	// P := []int{59, 60, 60, 59, 59, 59, 59, 60, 60, 60, 60, 60, 60, 59, 60, 60, 59}
+	// time := solve(P)
+	// fmt.Println(time)
+
+	n17 := factorial(float64(17))
+	n10 := factorial(float64(10))
+	n7 := factorial(float64(7))
+	s := fmt.Sprintf("%.6f", n17)
+	fmt.Println(s)
+	s = fmt.Sprintf("%.6f", n10)
+	fmt.Println(s)
+	s = fmt.Sprintf("%.6f", n7)
+	fmt.Println(s)
+
+	res := n17 / (n10 * n7)
+	s = fmt.Sprintf("%.6f", res)
+	fmt.Println(s)
+	//fmt.Println(factorial(float64(17) / factorial(float64(7)) * factorial(float64(10))))
 
 	// reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
