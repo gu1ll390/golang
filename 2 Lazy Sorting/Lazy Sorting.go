c0df2a4f416fc3a9734bc543608568ceaa0a6678
PRICETRAVEL\guillermo.fernandez <guillermo.fernandez@pricetravel.com>
2018-08-08 10:48:53 -0500

    Done! Lazy Sorting

--- 2 Lazy Sorting/Lazy Sorting.go
+++ 2 Lazy Sorting/Lazy Sorting.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -15,10 +17,11 @@ func solve(P []int) float64 {
 	}
 
 	sort.Ints(P)
-	n := factorial(float64(len(P)))
+	N := len(P)
+	n := factorial(float64(N))
 	div := float64(1)
 	sameValues := 1
-	for i := 0; i < len(P)-1; i++ {
+	for i := 0; i < N-1; i++ {
 		if P[i] == P[i+1] {
 			sameValues++
 		} else {
@@ -27,7 +30,11 @@ func solve(P []int) float64 {
 				sameValues = 1
 			}
 		}
+	}
 
+	if sameValues != 1 {
+		div = div * factorial(float64(sameValues))
+		sameValues = 1
 	}
 
 	return n / div
@@ -43,60 +50,36 @@ func factorial(n float64) float64 {
 
 func main() {
 
-	// P := []int{5, 2}
-	// //P := []int{39, 34, 33, 40, 34, 33}
-	// //P := []int{26, 9, 18, 18, 17, 12, 23, 23, 14, 29} //case 4
-	// //P := []int{5, 2, 19, 15, 19, 23, 6, 11, 2, 18, 6, 30, 28, 5, 12} //case 6
-	// P := []int{99, 98, 97, 97, 100, 95, 96, 91, 97, 99, 98, 95, 93, 90, 95, 97, 97, 91} //case 7
-	// P := []int{59, 60, 60, 59, 59, 59, 59, 60, 60, 60, 60, 60, 60, 59, 60, 60, 59}
-	// time := solve(P)
-	// fmt.Println(time)
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
 
-	n17 := factorial(float64(17))
-	n10 := factorial(float64(10))
-	n7 := factorial(float64(7))
-	s := fmt.Sprintf("%.6f", n17)
-	fmt.Println(s)
-	s = fmt.Sprintf("%.6f", n10)
-	fmt.Println(s)
-	s = fmt.Sprintf("%.6f", n7)
-	fmt.Println(s)
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
 
-	res := n17 / (n10 * n7)
-	s = fmt.Sprintf("%.6f", res)
-	fmt.Println(s)
-	//fmt.Println(factorial(float64(17) / factorial(float64(7)) * factorial(float64(10))))
+	defer stdout.Close()
 
-	// reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
 
-	// stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	// checkError(err)
+	PCount, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
 
-	// defer stdout.Close()
+	PTemp := strings.Split(readLine(reader), " ")
 
-	// writer := bufio.NewWriterSize(stdout, 1024*1024)
+	var P []int
 
-	// PCount, err := strconv.ParseInt(readLine(reader), 10, 64)
-	// checkError(err)
+	for i := 0; i < int(PCount); i++ {
+		PItemTemp, err := strconv.ParseInt(PTemp[i], 10, 64)
+		checkError(err)
+		PItem := int(PItemTemp)
+		P = append(P, PItem)
+	}
 
-	// PTemp := strings.Split(readLine(reader), " ")
+	result := solve(P)
 
-	// var P []int
+	s := fmt.Sprintf("%.6f", result)
 
-	// for i := 0; i < int(PCount); i++ {
-	// 	PItemTemp, err := strconv.ParseInt(PTemp[i], 10, 64)
-	// 	checkError(err)
-	// 	PItem := int(PItemTemp)
-	// 	P = append(P, PItem)
-	// }
+	fmt.Fprintf(writer, "%v\n", s)
 
-	// result := solve(P)
-
-	// s := fmt.Sprintf("%.6f", result)
-
-	// fmt.Fprintf(writer, "%v\n", s)
-
-	// writer.Flush()
+	writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
